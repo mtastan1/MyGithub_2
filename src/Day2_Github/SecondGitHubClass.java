3ffa9097545c816ee0a3297a275e0c69f600fb63
m.tastan <m.tastan@outlook.com>
2023-01-31 20:15:38 -0600

    same changes for conflict

--- src/Day2_Github/SecondGitHubClass.java
+++ src/Day2_Github/SecondGitHubClass.java
@@ -3,7 +3,15 @@ package Day2_Github;
 public class SecondGitHubClass {
     public static void main(String[] args) {
         System.out.println("Im learning GitHub...");
+
+
+        System.out.println();
+        System.out.println("Im creating a conflict");
+
+
+
         System.out.println("Im add login features");
+
     }
 
 }
